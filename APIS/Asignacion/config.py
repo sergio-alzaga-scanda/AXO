1d2f518c74879b1c8f8129541797e06199acfaab
Sergio Alzaga <sergio.alzaga@scanda.com.mx>
2026-02-03 15:36:54 -0600

    mod bd

--- APIS/Asignacion/config.py
+++ APIS/Asignacion/config.py
@@ -1,14 +1,14 @@
 BASE_URL = "https://servicedesk.grupoaxo.com/api/v3"
 API_KEY = "5E1B42AC-D6EE-4D82-850C-9AD9CE3C7C2D"
 
-MYSQL_HOST = "localhost"
-MYSQL_PORT = 3307
-MYSQL_USER = "root"
-MYSQL_PASSWORD = ""
-MYSQL_DB = "axo"
-
-# MYSQL_HOST = "158.23.137.150"
-# MYSQL_PORT = 3306
+# MYSQL_HOST = "localhost"
+# MYSQL_PORT = 3307
 # MYSQL_USER = "root"
-# MYSQL_PASSWORD = "Melco154.,"
+# MYSQL_PASSWORD = ""
 # MYSQL_DB = "axo"
+
+MYSQL_HOST = "158.23.137.150"
+MYSQL_PORT = 3306
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "Melco154.,"
+MYSQL_DB = "axo"
